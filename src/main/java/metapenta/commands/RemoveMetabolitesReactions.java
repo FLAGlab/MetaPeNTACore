f6c4b51b9a910d4eed8afd12e80e5015db639db0
jorge <jorge@jorge-OptiPlex-5090>
2024-06-01 14:39:30 -0500

    Added new services and improved networks with reactions for glycolysis and krebs cycle

--- src/main/java/metapenta/commands/RemoveMetabolitesReactions.java
+++ src/main/java/metapenta/commands/RemoveMetabolitesReactions.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import metapenta.model.networks.MetabolicNetwork;
 import metapenta.services.IMetabolicNetworkService;
 import metapenta.services.MetabolicNetworkService;
+import metapenta.services.SelectSubnetworkService;
 import metapenta.tools.io.writers.MetabolicNetworkXMLOutput;
 
 public class RemoveMetabolitesReactions {
@@ -19,25 +20,13 @@ public class RemoveMetabolitesReactions {
 
 		MetabolicNetwork network = networkService.getNetwork();
 		System.out.println("Loaded reactions: "+network.getReactionsAsList().size());
-		Set<String> metaboliteIds = loadIds(args[1]);
-		Set<String> reactionIds = loadIds(args[2]);
+		Set<String> metaboliteIds = SelectSubnetworkService.loadIds(args[1]);
+		Set<String> reactionIds = SelectSubnetworkService.loadIds(args[2]);
 		network.removeReactions(reactionIds);
 		network.removeMetabolites(metaboliteIds);
 		System.out.println("Remaining reactions: "+network.getReactionsAsList().size());
 		MetabolicNetworkXMLOutput writer = new MetabolicNetworkXMLOutput();
 		writer.saveNetwork(network, args[3]);
-	}
-
-	private static Set<String> loadIds(String filename) throws IOException {
-		Set<String> ids = new HashSet<>();
-		try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-			String line = reader.readLine();
-			while (line!=null) {
-				ids.add(line);
-				line = reader.readLine();
-			}
-		}
-		return ids;
 	}
 
 }
